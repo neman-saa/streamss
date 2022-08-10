5c58a01ffc862bd177253cf0eb2ea39767e37cd0
neman-saa <neman.saa@gmail.com>
2022-08-10 17:30:34 +0300

    Initial commit

---
+++ src/main/scala/streams/StringParserTerrain.scala
@@ -0,0 +1,83 @@
+package streams
+
+/**
+ * This component implements a parser to define terrains from a
+ * graphical ASCII representation.
+ *
+ * When mixing in that component, a level can be defined by
+ * defining the field `level` in the following form:
+ *
+ *   val level =
+ *     """------
+ *       |--ST--
+ *       |--oo--
+ *       |--oo--
+ *       |------""".stripMargin
+ *
+ * - The `-` character denotes parts which are outside the terrain
+ * - `o` denotes fields which are part of the terrain
+ * - `S` denotes the start position of the block (which is also considered
+     inside the terrain)
+ * - `T` denotes the final position of the block (which is also considered
+     inside the terrain)
+ *
+ * In this example, the first and last lines could be omitted, and
+ * also the columns that consist of `-` characters only.
+ */
+trait StringParserTerrain extends GameDef:
+
+  /**
+   * A ASCII representation of the terrain. This field should remain
+   * abstract here.
+   */
+  val level: String
+
+  /**
+   * This method returns terrain function that represents the terrain
+   * in `levelVector`. The vector contains parsed version of the `level`
+   * string. For example, the following level
+   *
+   *   val level =
+   *     """ST
+   *       |oo
+   *       |oo""".stripMargin
+   *
+   * is represented as
+   *
+   *   Vector(Vector('S', 'T'), Vector('o', 'o'), Vector('o', 'o'))
+   *
+   * The resulting function should return `true` if the position `pos` is
+   * a valid position (not a '-' character) inside the terrain described
+   * by `levelVector`.
+   */
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = {
+    def terrain(a: Pos): Boolean = {
+      def valueOf[T](a: Vector[T], b: Int): T = {
+        if b == 0 then a.head
+        else valueOf(a.tail, b-1)
+        
+      }
+      if valueOf(valueOf(levelVector, a._1), a._2) == 'o' then true else false}
+    terrain
+     }
+
+  /**
+   * This function should return the position of character `c` in the
+   * terrain described by `levelVector`. You can assume that the `c`
+   * appears exactly once in the terrain.
+   *
+   * Hint: you can use the functions `indexWhere` and / or `indexOf` of the
+   * `Vector` class
+   */
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = {
+    val a = levelVector.map(x => x.indexOf(c))
+    Pos(a.indexWhere(b => b>0), a.filter(b => b>0).head)
+  }
+
+  lazy val vector: Vector[Vector[Char]] =
+    Vector(level.split("\r?\n").map(str => Vector(str*)).toIndexedSeq*)
+
+  lazy val terrain: Terrain = terrainFunction(vector)
+  lazy val startPos: Pos = findChar('S', vector)
+  lazy val goal: Pos = findChar('T', vector)
+
